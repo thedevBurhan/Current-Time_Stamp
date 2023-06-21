5bdefd2ad553edf26bacb50aed73559569c7209b
thedevBurhan <smdburhan25@gmail.com>
2023-06-21 18:20:53 +0530

    Current time Stamp task

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-let a = 10 ;
+const express=require("express");
+const fs=require("fs");
+const path=require("path");
+const dirPath=path.join(__dirname,"TimeStamp");
+
+//  initilaize express server framework
+
+const app=express();
+
+app.get("/",(req,res)=>{
+   res.send("Hey there im working fine 🤣")
+})
+
+
+// Creating end Points
+app.get("/sample",(req,res)=>{
+   res.send("Hey, Sample End-Point working fine 😁")
+})
+
+// Creating Date-Time
+app.get("/date-time",(req,res)=>{
+  let date= new Date();
+  let currentTimeStamp=date.toUTCString().slice(0,-3);
+  let content=`The last updated timestamp :${currentTimeStamp}`;
+  let fileName=currentTimeStamp.split(" ").join("").split(",").join("").split(":").join("");
+ //console.log(dirPath);
+  fs.writeFile(`${dirPath}/${fileName}.txt`,content,(err)=>{
+   if(err){
+       res.send("Error in writing the File");      
+   }else{
+       res.sendFile(path.join(dirPath,"date-time.txt"));
+   }
+ })
+  
+})
+// listen to a server
+
+app.listen(9000,()=>console.log("Server started in Localhost:9000"))
